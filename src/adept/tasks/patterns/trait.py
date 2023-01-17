954070174217a8b4ae98c15d73dbd885c18d5369
Ben Scott <ben@benscott.co.uk>
2023-01-17 12:42:03 +0000

    update tasks to inherit from base task

--- src/adept/tasks/patterns/trait.py
+++ src/adept/tasks/patterns/trait.py
@@ -4,19 +4,20 @@ import luigi
 from adept.config import PROCESSED_DATA_DIR
 from adept.traits import Traits
 from adept.utils.patterns import Patterns
+from adept.tasks.base import BaseTask
 
 
-class TraitPatternsTask(luigi.Task):
+class TraitPatternsTask(BaseTask):
     
     def run(self):
         traits = Traits()
         patterns = Patterns()
 
         for term in traits.get_unique_discrete_terms():            
-            patterns.add(term, 'TRAIT')
+            patterns.add(term.strip(), 'TRAIT')
             
         for term in traits.get_unique_colour_terms():            
-            patterns.add(term, 'COLOUR')            
+            patterns.add(term.strip(), 'COLOUR')            
             
         patterns.to_jsonl(self.output().path)
          
@@ -25,4 +26,4 @@ class TraitPatternsTask(luigi.Task):
 
 
 if __name__ == "__main__":    
-    luigi.build([TraitPatternsTask()], local_scheduler=True)   
+    luigi.build([TraitPatternsTask(force=True)], local_scheduler=True)   
