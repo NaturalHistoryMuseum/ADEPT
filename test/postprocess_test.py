a76ef37d4a9a12b101382f694a45292d2a94a53e
Ben Scott <ben@benscott.co.uk>
2023-01-17 12:40:49 +0000

    add tests

---
+++ test/postprocess_test.py
@@ -0,0 +1,8 @@
+    #     pass
+    
+if __name__ == '__main__':
+    postprocess = Postproccess()
+    
+    
+    text = "Herbs to 50 cm tall, annual, much branched. Stems 4-angled, glabrous. Petiole 0.3-1 cm; leaf blade ovate-lanceolate, lanceolate, or narrowly elliptic, 1.5-7 × 1-2.5 cm, both surfaces glabrous, abaxially pale green, adaxially green, secondary veins 3-5 on each side of midvein, base attenuate and decurrent onto petiole, margin entire, apex acute to shortly acuminate."
+    # print(traits.is_description(text))
