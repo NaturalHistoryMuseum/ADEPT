f1ed445373e47021c510570836a9c5894f9ca737
Ben Scott <ben@benscott.co.uk>
2023-01-24 17:39:46 +0000

    Add componets script

---
+++ scripts/components.py
@@ -0,0 +1,51 @@
+import spacy
+from spacy.language import Language
+from spacy.tokens import Doc
+
+
+# from adept.components.registry import create_sentencizer_component
+
+class SentencizerComponent:
+    
+    """
+    Sentencizer, to split sentences on semicolons and periods.
+    
+    If we just add is_sent_start for each semicolon, the default
+    parser will split incorrectly
+    """
+    
+    def __init__(self, nlp: Language):
+        self.nlp = nlp
+    
+    def __call__(self, doc):        
+        for token in doc[1:]:
+            if self._is_sent_end(doc, doc[token.i - 1]):
+                token.is_sent_start = True
+            else:
+                token.is_sent_start = False
+                
+        return doc
+    
+    def _is_sent_end(self, doc, token):
+        return any([
+            self._is_semicolon(doc, token),
+            self._is_period(doc, token),
+        ])
+    
+    @staticmethod
+    def _is_semicolon(doc, token):
+        return token.text == ";"
+    
+    @staticmethod
+    def _is_period(doc, token):
+        next_token = doc[token.i + 1]                
+        # Ensure the next word starts with a capital or a number      
+        return token.text == "." and (next_token.shape_.startswith('X')) 
+
+
+
+@Language.factory("semicolon_sentencizer")
+def create_sentencizer_component(nlp: Language, name: str):
+    return SentencizerComponent(nlp)
+
+
