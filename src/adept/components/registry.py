bb43e518a8c7859bea366bbfe85ae2b8ee331f23
Ben Scott <ben@benscott.co.uk>
2023-01-24 17:38:45 +0000

    Update src files

--- src/adept/components/registry.py
+++ src/adept/components/registry.py
@@ -1,5 +1,18 @@
 import spacy
 from spacy.language import Language
+from spacy.tokens import Doc
+
+
+from adept.components.sentencizer import SentencizerComponent
+
+
+@Language.component("semicolon_sentencizer")
+def create_sentencizer_component(nlp: Language):
+    return SentencizerComponent(nlp)
+
+
+
+# OLD: To remove
 
 class ComponentsRegistry(object):
     def __init__(self, nlp):
@@ -10,7 +23,6 @@ class ComponentsRegistry(object):
             self._register_component(component) 
 
         for component in components:  
-            # if Language.has_factory(component.name):
             self._update_pipeline_component(component) 
                 
     def add_component(self, component):    
