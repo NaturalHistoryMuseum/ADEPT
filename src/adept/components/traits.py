dad4faacba9d6437258483d3afbe45619dac496d
Ben Scott <ben@benscott.co.uk>
2023-01-30 20:10:51 +0000

    Fix ploidy 2n

--- src/adept/components/traits.py
+++ src/adept/components/traits.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from spacy.util import filter_spans
 
 from adept.utils.expand import ExpandSpan
+from adept.utils.helpers import token_get_ent
 from adept.tasks.patterns.trait import TraitPatternsTask
 
 class DiscreteTraitsComponent(EntityRuler):
@@ -24,31 +25,31 @@ class DiscreteTraitsComponent(EntityRuler):
         self.from_disk(self.patterns_file_path)
         
         
-class NumericTraitsComponent:  
+class CustomTraitsComponent:  
     
     def __init__(self, nlp):                 
 
         self.matcher = Matcher(nlp.vocab)   
         self.matcher.add('PLOIDY LEVEL (2n)', [[{"LOWER": "2n"}, {"LOWER": "="}]], on_match=self.on_ploidy_match)
+        self.matcher.add('PLOIDY LEVEL (2n)', [[{"TEXT": {"REGEX": "2n="}}]], on_match=self.on_ploidy_match)
         self.matcher.add('MEROSITY', [[{"POS": "NUM"}, {"LOWER": "-"}, {"LOWER": "merous"}]], on_match=self.on_merosity_match)
         
         self.expand_ploidy = ExpandSpan(
             ['=', ','], 
-            pos_tags=['NUM'],
-            entity_types=['CARDINAL']
+            pos_tags=['NUM']
         )
         Span.set_extension("trait_value", default=None, force=True)
 
     def __call__(self, doc):
-        self.ents = list(doc.ents)
+        self.ents = set(doc.ents)
         self.matcher(doc)
-        doc.ents = filter_spans(self.ents)
+        doc.ents = filter_spans(list(self.ents))
         return doc  
         
     def on_ploidy_match(self, matcher, doc, i, matches): 
         match_id, start, end = matches[i]
         token = doc[start]
-        span = self.expand_ploidy(doc, token)
+        span = self.expand_ploidy(doc, token)       
         self._add_ent(doc, span.start, span.end, match_id)
                 
     def on_merosity_match(self, matcher, doc, i, matches): 
@@ -58,6 +59,10 @@ class NumericTraitsComponent:
     
     def _add_ent(self, doc, start, end, match_id):
         string_id = self.matcher.vocab.strings[match_id]
-        entity = Span(doc, start, end, label=string_id)        
+        entity = Span(doc, start, end, label=string_id)                
+        overlapping = {ent for token in entity if (ent := token_get_ent(token))}
+        # Rather than relying on filter_spans, explicity remove existing ents on the tokens
+        # otherwise some CARDINAL ents will be longer and preferred to ploidy level
+        self.ents = self.ents.difference(overlapping)        
         entity._.set("trait_value", entity.text)                   
-        self.ents.append(entity)            
+        self.ents.add(entity)            
