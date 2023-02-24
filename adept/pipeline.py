bbbabb0a9b489c5ee54caaf95cdd33ced7bab024
Ben Scott <ben@benscott.co.uk>
2023-02-24 23:03:53 +0000

    Order components

--- adept/pipeline.py
+++ adept/pipeline.py
@@ -3,7 +3,7 @@ import spacy
 from adept.components.registry import ComponentsRegistry
 from adept.preprocess import Preprocess
 from adept.postprocess import Postproccess
-from adept.config import TRAINING_DIR
+from adept.config import MODEL_DIR
 
 
 class Pipeline():
@@ -11,13 +11,13 @@ class Pipeline():
     def __init__(self):
 
         # self.nlp = spacy.load("en_core_web_trf")        
-        self.nlp = spacy.load(TRAINING_DIR / 'adept')
+        self.nlp = spacy.load(MODEL_DIR / 'adept')
         
         registry = ComponentsRegistry(self.nlp)        
         registry.add_component('numeric')
         registry.add_component('anatomical_ner')
-        registry.add_component('traits_ner')
-        registry.add_component('traits_custom_ner')
+        registry.add_component('traits_ner', after="anatomical_ner")
+        registry.add_component('traits_custom_ner', after="traits_ner")
         registry.add_component('dimension_ner')
         registry.add_component('measurement_rel', after="dimension_ner")
                 
@@ -25,7 +25,7 @@ class Pipeline():
         self.postprocess = Postproccess()  
 
     
-    def __call__(self, text, taxon_group):                  
+    def __call__(self, text, taxon_group):            
         text = self.preprocess(text)          
         self.doc = self.nlp(text)
         return self.postprocess(self.doc, taxon_group)
